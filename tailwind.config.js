ca8ff65483ff402ba5b7bd7ad054bf2a82296bef
Beatriz <beatrizferreirapinto@hotmail.com>
2022-09-17 14:16:13 -0300

    Tailwind config space

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,15 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        '100': '25rem',
+        '104': '26rem',
+        '108': '27rem',
+        '112': '28rem',
+      }
+    }
+  }
 }
