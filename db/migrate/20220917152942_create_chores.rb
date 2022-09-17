c74571b72152ebd63e6deb13b97885c7567f36f2
Beatriz <beatrizferreirapinto@hotmail.com>
2022-09-17 12:33:07 -0300

    create table chore

---
+++ db/migrate/20220917152942_create_chores.rb
@@ -0,0 +1,10 @@
+class CreateChores < ActiveRecord::Migration[7.0]
+  def change
+    create_table :chores do |t|
+      t.string :title, :null => false
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
