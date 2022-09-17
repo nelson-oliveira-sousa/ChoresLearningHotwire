84c54c09ab34160c291eace7ab72fe797abd938f
Beatriz <beatrizferreirapinto@hotmail.com>
2022-09-17 16:59:44 -0300

    broadcast

--- app/controllers/chores_controller.rb
+++ app/controllers/chores_controller.rb
@@ -1,4 +1,8 @@
 class ChoresController < ApplicationController
+  before_action :set_chore, only: %i[ show edit update destroy ]
+  after_action :broadcast_insert, only: %i[create]
+  after_action :broadcast_remove, only: %i[destroy]
+
   def index
     @chores = Chore.all
     @chore = Chore.new
@@ -19,8 +23,39 @@ class ChoresController < ApplicationController
     end
   end
 
+  def destroy
+    @chore.destroy
+    respond_to do |format|
+      format.turbo_stream # include this
+      format.html { redirect_to chores_url, notice: "Chore was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
 
   private
+
+  def broadcast_insert
+    return if @chore.errors.any?
+    Turbo::StreamsChannel.broadcast_append_to(
+      "chores",
+      target: "chores",
+      partial: "chores/chore",
+      locals: { chore: @chore }
+    )
+  end
+
+  def broadcast_remove
+    return unless @chore.destroyed?
+    Turbo::StreamsChannel.broadcast_remove_to(
+      "chores",
+      target: ActionView::RecordIdentifier.dom_id(@chore)
+    )
+  end
+
+  def set_chore
+    @chore = Chore.find(params[:id])
+  end
 
   def chore_params
     params.require(:chore).permit(:title, :content)
